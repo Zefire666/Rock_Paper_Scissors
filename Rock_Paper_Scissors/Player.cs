43847a78e3c1c382979237729bb3498995dc2d40
Zefire666 <dancerbonia@gmail.com>
2023-07-20 17:51:52 +0300

    First version

---
+++ Rock_Paper_Scissors/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training;
+    public class Player
+    {
+    public enum VARIANTS { ROCK, PAPER, SCISSORS };
+
+    private string name;
+    public VARIANTS choice;
+
+    public Player()
+    {
+        name = "Bot";
+        Random random = new Random();
+        choice = (VARIANTS)random.Next(0, 3);
+    }
+
+    public Player(VARIANTS choice, string name)
+    {
+        this.name = name;
+        this.choice = choice;
+    }
+
+    public string whoWins(Player p1, Player p2)
+    {
+        if (p1.choice == p2.choice)
+        {
+            return "Draw";
+        }
+        else if (p1.choice == VARIANTS.ROCK && p2.choice == VARIANTS.SCISSORS ||
+            p1.choice == VARIANTS.PAPER && p2.choice == VARIANTS.ROCK ||
+            p1.choice == VARIANTS.SCISSORS && p2.choice == VARIANTS.PAPER)
+        {
+            return $"Победил игрок с именем: {p1.name}";
+        }
+        else
+        {
+            return $"Победил игрок с именем: {p2.name}";
+        }
+    }
+    }
---
+++ Rock_Paper_Scissors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+using static Training.Player;
+
+namespace Training;
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        Player bot = new Player();
+        Player alex = new Player(VARIANTS.PAPER, "Alex");
+
+        Console.WriteLine(bot.whoWins(bot, alex));
+
+        Console.ReadLine();
+
+
+    }
+    }
